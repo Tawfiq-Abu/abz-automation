b41de7eb1c98d061bdda3f32f328d7dd07a629c6
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2022-07-30 12:20:21 +0000

    feat:registered models for the teammember

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TeamMember
 
 # Register your models here.
+@admin.site.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display= ['firstname','othername', 'lastname','role','image','linked_in','email']
