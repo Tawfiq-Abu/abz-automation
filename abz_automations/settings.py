061d6ff1d79fc3bcfbcf7d819c4a72290dac5a09
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2022-09-05 19:42:00 +0000

    hotfix:some error

--- abz_automations/settings.py
+++ abz_automations/settings.py
@@ -86,16 +86,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'abz_automations.wsgi.application'
 
+PLATFORM = config('PLATFORM')
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if PLATFORM == "local":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True),
+    }
 
 
 # Password validation
