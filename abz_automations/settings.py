a4d6702a9ca668648424c5c55baff7dcd7e58b9b
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2022-09-05 20:10:35 +0000

    hotfix:some error

--- abz_automations/settings.py
+++ abz_automations/settings.py
@@ -104,7 +104,7 @@ else:
     DATABASES = {
         'default': dj_database_url.config(conn_max_age=600, ssl_require=True),
     }
-
+# hmm
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
