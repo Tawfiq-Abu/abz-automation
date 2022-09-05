b2606f09f052365472a1981b590cd8a9f2aab869
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2022-09-05 18:15:50 +0000

    hotfix:some error

--- abz_automations/settings.py
+++ abz_automations/settings.py
@@ -29,7 +29,17 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://abz-automations.herokuapp.com/"
+]
+
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "localhost",
+        '127.0.0.1',
+        '0.0.0.0',
+        ]
 
 
 # Application definition
