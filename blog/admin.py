a0416d6ee38f07045743a4d5b1f9e64420c430a4
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2022-10-11 23:04:35 +0000

    feat:registered models with admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Blog
 
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "is_active"]
+    list_filter = ["title", "author"]
+    search_fields= ["title", "author"]
+
+
+admin.site.register(Blog,BlogAdmin)
