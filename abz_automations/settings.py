8fd6356795bd6aaaea4f285b8e663f83bbd766f4
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2022-10-11 23:01:32 +0000

    feat:added blog app and did models

--- abz_automations/settings.py
+++ abz_automations/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'website',
     'location_field.apps.DefaultConfig',
     'storages',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -173,12 +174,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # }
 
 # Emailing
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.yourserver.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = ('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.yourserver.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = ('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = True
 
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default="")
---
+++ blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+import readtime
+from uuslug import uuslug
+
+
+def blogs_image_directory(instance, filename):
+    return '/'.join(['images', 'team-member', str(instance.first_name), filename])
+
+
+# Create your models here.
+class Blog(models.Model):
+    # author = models.ForeignKey(User, on_delete=models.SET(get_default_user))
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to=blogs_image_directory)
+    description = RichTextUploadingField()
+    # topics = models.ManyToManyField(Topic, related_name="blogs")
+    slug = models.SlugField(max_length=256,unique=True,editable=False)
+    is_active = models.BooleanField(default=False)
+    views = models.BigIntegerField(default=0)
+    
+    def save(self,*args,**kwargs):
+        self.slug = uuslug(self.title, instance=self)
+        super().save(*args,**kwargs)
+    
+    class Meta:
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self):
+        return self.title
+    
+    def increase_views(self):
+        self.views += 1
+        self.save()
+    
+    def get_duration(self):
+        return str(readtime.of_html(self.description)).capitalize()
