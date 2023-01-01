d53f8852e44720c6d47bd7e432d1319cfa082636
Machele-codez <smithbeblack@gmail.com>
2023-01-01 00:23:20 +0000

    some formatting

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from .models import Blog
 
 # Register your models here.
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ["title", "slug", "is_active"]
+    list_display = ["title", "is_active"]
     list_filter = ["title"]
     search_fields= ["title"]
 
